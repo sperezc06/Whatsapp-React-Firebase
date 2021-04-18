860e2a77929b676db34b21edc761bf153da12a3f
sperezc06 <sperezc06@gmail.com>
2021-04-18 15:58:22 -0500

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB088aW1UTluW97wrUBluJyyNpoAh26Sj0",
-  authDomain: "whatsapp-clone-e2938.firebaseapp.com",
+  authDomain: "https://sperezc06.github.io/Whatsapp-React-Firebase/",
   databaseURL: "https://whatsapp-clone-e2938-default-rtdb.firebaseio.com",
   projectId: "whatsapp-clone-e2938",
   storageBucket: "whatsapp-clone-e2938.appspot.com",
@@ -10,6 +10,8 @@ const firebaseConfig = {
   appId: "1:317514429795:web:5afc70777975826b950f56",
   measurementId: "G-80ZJVH6D53",
 };
+
+// whatsapp - clone - e2938.firebaseapp.com;
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
